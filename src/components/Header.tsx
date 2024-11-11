9b75a6a179d79a15dfdbdc26080eccec91427e2e
Ramy <ramy.magdy@rockettravelsystem.com>
2024-11-11 16:35:17 +0200

    logo problem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,11 +15,7 @@ export function Header({ toggleLanguage, isArabic }: HeaderProps) {
     <header className="container mx-auto px-4 py-8">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <img
-            src="/img/logo.png"
-            alt="Hurghada Radio"
-            className="h-12 w-auto"
-          />
+          <img src="/logo.png" alt="Hurghada Radio" className="h-12 w-auto" />
         </div>
 
         {/* Mobile Menu Button */}
